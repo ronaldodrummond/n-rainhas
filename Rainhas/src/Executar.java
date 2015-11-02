5b85e3b94b706f9f1cca5ac24ef6728fdf70d4c8
Ronaldo <drummondronaldo@gmail.com>
2015-11-02 21:12:26 -0200

    N-Rainhas versão final 1.1

--- Rainhas/src/Executar.java
+++ Rainhas/src/Executar.java
@@ -8,30 +8,39 @@ public class Executar {
         Encosta encosta;
         Genetico genetico;
         Hibrido hibrido;
-        System.out.println("\n N-Rainhas \n");
-        System.out.print("\n1-Subida de Encosta\n2-Genético\n3-Híbrido\nDigite a opção desejada: ");
-        Scanner ler = new Scanner(System.in);
-        op = ler.nextInt();
-        System.out.print("Digite o tamanho do tabuleiro: ");
-        tamanho = ler.nextInt();   
+
+        do{
+            System.out.println("\n N-Rainhas \n");
+            System.out.print("\n1-Subida de Encosta\n2-Genético\n3-Híbrido\n4-Sair\nDigite a opção desejada: ");
+            Scanner ler = new Scanner(System.in);
+            op = ler.nextInt();
         
-        tabuleiro = new Tabuleiro(tamanho);
-        
-        switch (op){
-            case 1:
-                encosta = new Encosta(tabuleiro);
-                encosta.executar(); 
-                break;
-            case 2:
-                genetico = new Genetico(tamanho);
-                genetico.mutacao(tamanho);
-                break;
-            case 3:
-                //hibrido = new Hibrido(tamanho);
-                break;
-            default:
-                System.out.println("Opção invalida!");
-                break;                
-        }
+            switch (op){
+                case 1:
+                    System.out.print("Digite o tamanho do tabuleiro: ");
+                    tamanho = ler.nextInt();   
+                    tabuleiro = new Tabuleiro(tamanho);
+                    encosta = new Encosta(tabuleiro);
+                    encosta.executar(); 
+                    break;
+                case 2:
+                    System.out.print("Digite o tamanho do tabuleiro: ");
+                    tamanho = ler.nextInt(); 
+                    genetico = new Genetico(tamanho);
+                    genetico.mutacao(tamanho);
+                    break;
+                case 3:
+                    System.out.print("Digite o tamanho do tabuleiro: ");
+                    tamanho = ler.nextInt(); 
+                    hibrido = new Hibrido(tamanho);
+                    break;
+                case 4:
+                    System.out.println("\nSaindo...");
+                    break;
+                default:
+                    System.out.println("\nErro: Opção incorreta!"); 
+                    break;
+            }
+        }while(op!=4);
     }   
 }
